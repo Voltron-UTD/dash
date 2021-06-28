12541159a505599a5361728a8de98eb5763479ba
Will Heitman <will.heitman@utdallas.edu>
2021-06-27 21:31:13 -0500

    Add pointcloud layer from clipboard

--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ app.post('/api/pcd-upload', (req, res) => {
   let form = new multiparty.Form();
   form.parse(req, (err, fields, files) => {
     res.writeHead(200, { 'content-type': 'text/plain' });
-    res.write('received upload:\n\n');
+    //res.write('received upload:\n\n');
     res.end(util.inspect({ fields: fields, files: files }));
   });
 
--- src/app/components/mapping-helper/mapping-helper.component.ts
+++ src/app/components/mapping-helper/mapping-helper.component.ts
@@ -5,14 +5,17 @@ import '@cds/core/file/register.js';
 import { ClrTimelineStep, ClrTimelineStepState } from '@clr/angular';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
+import { GridLayer } from 'leaflet';
+import { GeoJsonObject } from 'geojson';
 
 @Component({
   selector: 'app-mapping-helper',
   templateUrl: './mapping-helper.component.html',
-  styleUrls: ['./mapping-helper.component.css']
+  styleUrls: ['./mapping-helper.component.css'],
 })
+
 export class MappingHelperComponent implements AfterViewInit {
-  private map: any;
+  private map: any | L.Map;
   zoneStepState: ClrTimelineStepState;
   originStepState: ClrTimelineStepState;
   pcdStepState: ClrTimelineStepState;
@@ -20,116 +23,221 @@ export class MappingHelperComponent implements AfterViewInit {
   canProgress: boolean = true;
 
   uploadedFiles: any | FileList;
-  
-  origin: L.LatLng | undefined;
+  pcdText: string = '';
+
+  origin: L.LatLng = L.latLng(-1,-1); //init to bogus number
   originMarker: L.Marker | undefined;
-  
-  constructor(private http: HttpClient) { 
-    ClarityIcons.addIcons(cogIcon)
+  jsonLayer: L.GeoJSON = new L.GeoJSON();
+
+  constructor(private http: HttpClient) {
+    ClarityIcons.addIcons(cogIcon);
     this.zoneStepState = ClrTimelineStepState.CURRENT;
     this.originStepState = ClrTimelineStepState.NOT_STARTED;
     this.pcdStepState = ClrTimelineStepState.NOT_STARTED;
     this.mapFrozen = false;
   }
-  
+
   public get mapCursor(): string {
-    if(this.originStepState === ClrTimelineStepState.CURRENT) {
-      return "cell"
+    if (this.originStepState === ClrTimelineStepState.CURRENT) {
+      return 'cell';
     } else {
-      return ""
+      return '';
     }
   }
 
   public get mapBoundaryString(): string {
-    if (this.map)
-      return this.map.getBounds().toBBoxString();
-    else return "unset"
+    if (this.map) return this.map.getBounds().toBBoxString();
+    else return 'unset';
   }
-  
+
   public isCurrent(state: ClrTimelineStepState): boolean {
     return state === ClrTimelineStepState.CURRENT;
   }
-  
+
   ngAfterViewInit(): void {
     this.initMap();
   }
-  
+
+  public addMarkers(map: L.Map, points: L.LatLng[]) {
+    for (const point of points) {
+      // const marker = new L.Marker([point.lat, point.lng],
+      //   {
+      //     icon: new L.Icon
+      //   }
+      // );
+      const marker = L.circleMarker(point, {
+        radius: 5
+      });
+      marker.addTo(map);
+    }
+  }
+
   private initMap(): void {
     this.map = L.map('map', {
       center: [32.9865279, -96.7486954],
-      zoom: 15
+      zoom: 15,
     });
-    
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    minZoom: 3,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
-  
-  tiles.addTo(this.map);
-  
-  this.map.on('click', (e: any)=> {
-    this.mapClicked(e);
-  })
-}
 
-setMapOrigin(point: L.LatLng): void {
-  if (this.originMarker === undefined) {
-    this.originMarker = L.marker(point).addTo(this.map);
-  } else {
-    this.originMarker.remove();
-    this.originMarker = L.marker(point).addTo(this.map);
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+    this.map.addLayer(this.jsonLayer);
+
+    this.map.on('click', (e: any) => {
+      this.mapClicked(e);
+    });
   }
-  
-  this.origin = point;
-}
 
-public mapClicked(e: L.LeafletMouseEvent): void {
-  console.log(e);
-  
-  if (this.isCurrent(this.originStepState)) {
-    this.setMapOrigin(e.latlng);
+  setMapOrigin(point: L.LatLng): void {
+    if (this.originMarker === undefined) {
+      this.originMarker = L.marker(point).addTo(this.map);
+    } else {
+      this.originMarker.remove();
+      this.originMarker = L.marker(point).addTo(this.map);
+    }
+
+    this.origin = point;
+    this.canProgress = true;
   }
-}
 
-public freezeMap(): void {
-  this.mapFrozen = true;
-}
+  public mapClicked(e: L.LeafletMouseEvent): void {
+    console.log(e);
 
-fileChange(element: any) {
-  this.uploadedFiles = element.target.files;
-  console.log(this.uploadedFiles);
-}
+    if (this.isCurrent(this.originStepState)) {
+      this.setMapOrigin(e.latlng);
+    }
 
-get canUpload(): boolean {
-  if (this.uploadedFiles)
-    return this.uploadedFiles.length > 0;
-  else return false;
-}
+    // var geojson = 
+    //   {
+    //     type: "Feature",
+    //     geometry: {
+    //       type: "Point",
+    //       coordinates: [e.latlng.lat, e.latlng.lng]
+    //     },
+    //     properties: {} // Not optional for a Feature
+    //   }
 
-uploadPcdFile(): void {
-  let formData = new FormData();
-  for (var i = 0; i < this.uploadedFiles.length; i++) {
-    formData.append("uploads[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
-}
-  this.http.post('/api/pcd-upload', formData)
-  .subscribe((response) => {
+    //this.addMarkers(this.map, [e.latlng]);
+  }
+
+  public freezeMap(): void {
+    this.mapFrozen = true;
+  }
+
+  fileChange(element: any) {
+    this.uploadedFiles = element.target.files;
+    console.log(this.uploadedFiles);
+  }
+
+  get canUpload(): boolean {
+    if (this.uploadedFiles) return this.uploadedFiles.length > 0;
+    else return false;
+  }
+
+  uploadPcdFile(): void {
+    let formData = new FormData();
+    for (var i = 0; i < this.uploadedFiles.length; i++) {
+      formData.append(
+        'uploads[]',
+        this.uploadedFiles[i],
+        this.uploadedFiles[i].name
+      );
+    }
+    this.http.post('/api/pcd-upload', formData).subscribe((response) => {
       console.log('response received is ', response);
-  })
-}
-
-nextClicked(): void {
-  if (this.zoneStepState === ClrTimelineStepState.CURRENT) {
-    console.log(this.map.getBounds())
-    this.zoneStepState = ClrTimelineStepState.SUCCESS;
-    this.originStepState = ClrTimelineStepState.CURRENT;
-    this.freezeMap();
-  } else if (this.originStepState === ClrTimelineStepState.CURRENT) {
-    this.originStepState = ClrTimelineStepState.SUCCESS;
-    this.pcdStepState = ClrTimelineStepState.CURRENT;
-    this.canProgress = false;
+    });
   }
-  
+
+  public pasteFromClipboard(): void {
+    navigator.clipboard.readText().then((clipText) => {
+      this.pcdText = clipText;
+      //console.log(this.pcdText);
+      this.processPcdText(this.pcdText);
+    });
+  }
+
+  public getLatLngPoint(x: number, y: number, lat_per_m: number, lng_per_m: number): L.LatLng {
+    let lat = this.origin.lat+y*lat_per_m;
+    let lng = this.origin.lng+x*lng_per_m;
+    //console.log(y+", "+lat_per_m);
+    return L.latLng(lat, lng);
+  }
+
+  // TODO: Fix all of this to adapt to different PCD configs (e.g. different fields)
+  public processPcdText(data: string): void {
+    let substrings: string[] = data.split(/\r?\n/).splice(11);
+
+    // Calculate the precise lat-lon to x-y conversion relative to
+    // the origin. See https://en.wikipedia.org/wiki/Geographic_coordinate_system#Length_of_a_degree
+    let origin_lat = this.origin.lat*2*Math.PI/360
+    let degrees_lat_per_meter = 1/(111132.92-559.82*Math.cos(2*origin_lat)+1.175*Math.cos(4*origin_lat)-0.0023*Math.cos(6*origin_lat));
+    let degrees_lng_per_meter = 1/(111412.84*Math.cos(origin_lat)-93.5*Math.cos(3*origin_lat)+0.118*Math.cos(5*origin_lat));
+    console.log(degrees_lng_per_meter);
+
+    let latlngs: L.LatLng[] = [];
+    console.log(substrings);
+
+    for(var pointstr of substrings) {
+      const THRESHOLD = 0.05;
+      if (Math.random() < THRESHOLD) {
+        let elems: string[] = pointstr.split(' ');
+        console.log(pointstr);
+        if (elems.length===4){
+          let latlng = this.getLatLngPoint(parseFloat(elems[0]),parseFloat(elems[1]),degrees_lat_per_meter, degrees_lng_per_meter);
+          latlngs.push(latlng);
+        }
+      }
+      
+      
+    }
+
+    this.addMarkers(this.map, latlngs);
+
+   
+  }
+
+  nextClicked(): void {
+    if (this.zoneStepState === ClrTimelineStepState.CURRENT) {
+      console.log(this.map.getBounds());
+      this.zoneStepState = ClrTimelineStepState.SUCCESS;
+      this.originStepState = ClrTimelineStepState.CURRENT;
+      this.freezeMap();
+      this.canProgress = false;
+    } else if (this.originStepState === ClrTimelineStepState.CURRENT) {
+      this.originStepState = ClrTimelineStepState.SUCCESS;
+      this.pcdStepState = ClrTimelineStepState.CURRENT;
+      this.canProgress = false;
+    }
+  }
 }
 
+class DebugCoords extends L.GridLayer {
+  public createTile (coords: L.Coords) {
+    var tile = document.createElement('canvas');
+
+    var tileSize = this.getTileSize();
+    tile.setAttribute('width', tileSize.x.toString());
+    tile.setAttribute('height', tileSize.y.toString());
+
+    var ctx = tile.getContext('2d');
+
+    // Draw whatever is needed in the canvas context
+    // For example, circles which get bigger as we zoom in
+    if (ctx !== null) {
+      ctx.beginPath();
+      ctx.arc(tileSize.x/2, tileSize.x/2, 4 + coords.z*4, 0, 2*Math.PI, false);
+      ctx.fill();
+
+      
+    }
+    return tile;
+  }
 }
